023d11442bbc0add8c324782586741a486d8a337
priyank <priyank@codewithpriyank.com>
2024-12-18 07:03:13 +0530

    home and room done

--- chatbot/base/views.py
+++ chatbot/base/views.py
@@ -5,14 +5,19 @@ from django.shortcuts import render
 rooms = [
     {'id' : 1, 'name': 'Lets learn Python'},
     {'id' : 2, 'name': 'I am django developer'},
-    {'id' : 1, 'name': 'I am Data Scientist'},
+    {'id' : 3, 'name': 'I am Data Scientist'},
 ]
 
 def home(request):
-    # context = {'rooms': rooms}
-    return render(request, 'home.html', {'rooms': rooms})
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request,'base/room.html', context)
 
 
