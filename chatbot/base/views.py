dcdfee36d8991b89fbd976254c20dd1ae8ae1425
priyank <priyank@codewithpriyank.com>
2024-12-18 08:02:33 +0530

    create operation done

--- chatbot/base/views.py
+++ chatbot/base/views.py
@@ -1,23 +1,40 @@
 from django.shortcuts import render
+from .models import Room
 
 
+# rooms = [
+#     {'id' : 1, 'name': 'Lets learn Python'},
+#     {'id' : 2, 'name': 'I am django developer'},
+#     {'id' : 3, 'name': 'I am Data Scientist'},
+# ]
 
-rooms = [
-    {'id' : 1, 'name': 'Lets learn Python'},
-    {'id' : 2, 'name': 'I am django developer'},
-    {'id' : 3, 'name': 'I am Data Scientist'},
-]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id = pk)
     context = {'room': room}
     return render(request,'base/room.html', context)
 
+
+def createRoom(request):
+    # form = RoomForm()
+    # topics = Topic.objects.all()
+    # if request.method == 'POST':
+    #     topic_name = request.POST.get('topic')
+    #     topic, created = Topic.objects.get_or_create(name = topic_name)
+        
+    #     Room.objects.create(
+    #         host = request.user,
+    #         topic = topic,
+    #         name = request.POST.get('description'),
+    #     )
+    #     return redirect('home')
+        
+    # context = {'form': form, 'topics': topics}
+    context = {}
+    return render(request, 'base/room_form.html', context)
 
