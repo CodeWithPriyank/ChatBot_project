294a6c887475dd09a108ed7e9b4b12669658ba9d
priyank <priyank@codewithpriyank.com>
2024-12-18 06:45:21 +0530

    home page added

--- chatbot/base/views.py
+++ chatbot/base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
+
+rooms = [
+    {'id' : 1, 'name': 'Lets learn Python'},
+    {'id' : 2, 'name': 'I am django developer'},
+    {'id' : 1, 'name': 'I am Data Scientist'},
+]
+
 def home(request):
-    return render(request,'home.html')
+    # context = {'rooms': rooms}
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request,'room.html')
