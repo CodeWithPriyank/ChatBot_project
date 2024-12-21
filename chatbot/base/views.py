150ec10f350e63593c6c64481992fc4d7159f55f
priyank <priyank@codewithpriyank.com>
2024-12-22 02:09:34 +0530

    comment section done

--- chatbot/base/views.py
+++ chatbot/base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Room, Topic, User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, User, Message
 from .forms import RoomForm
 
 # rooms = [
@@ -12,12 +15,12 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
-    # page = 'login'
-    # if request.user.is_authenticated:
-    #     return redirect('home')
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         
         try:
@@ -33,14 +36,31 @@ def loginPage(request):
         else:
             messages.error(request, "Username or password does not exist") 
         
-    # context = {'page': page}
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
+
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
 
+
+def registerPage(request):
+    form = UserCreationForm() 
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False) #commit = false because hume direct user register karne ke baad info chaiye agar user ne upprer case me email dala hai toh hum usse lower case me convert karenge 
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occured during registration")
+    
+    return render(request, 'base/login_register.html', {'form': form})
+    
+    
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
@@ -55,10 +75,20 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created') #to get messages of speicific room
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk = room.id)
+    
+    context = {'room': room, 'room_messages': room_messages}
     return render(request,'base/room.html', context)
 
-
+@login_required(login_url = '/login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -83,20 +113,23 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url = '/login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance = room)
     # topics = Topic.objects.all()
     
+        
+    if request.user != room.host:
+       return HttpResponse('You are not allowed here!!') 
+   
     if request.method == 'POST':
         form = RoomForm(request.POST, instance = room)
         if form.is_valid():
             form.save()
             return redirect('home')
         
-    # if request.user != room.host:
-    #    return HttpResponse('You are not allowed here!!') 
-   
+        
     # if request.method == 'POST':
     #     topic_name = request.POST.get('topic')
     #     topic, created = Topic.objects.get_or_create(name = topic_name)
@@ -110,6 +143,7 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url = '/login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
 
