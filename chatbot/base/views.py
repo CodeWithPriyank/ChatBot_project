d6c158f2b9c4a69f5c8663a7a1be9345508fc295
priyank <priyank@codewithpriyank.com>
2024-12-24 07:03:55 +0530

    edit profile page done

--- chatbot/base/views.py
+++ chatbot/base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, User, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # rooms = [
 #     {'id' : 1, 'name': 'Lets learn Python'},
@@ -163,3 +163,16 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk = user.id)
+    
+    return render(request, 'base/update_user.html', {'form': form})
